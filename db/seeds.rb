7eef938dfa267ede2a61b27661a17a4f248fc605
Jon Goodger <jonno.is@gmail.com>
2014-12-11 20:17:15 +0000

    Seed database with common terms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Term.create english: "double crochet", american: "single crochet"
+Term.create english: "dc", american: "sc"
+Term.create english: "half treble", american: "half double crochet"
+Term.create english: "htr", american: "hdc"
+Term.create english: "treble", american: "double crochet"
+Term.create english: "tr", american: "dc"
+Term.create english: "double treble", american: "treble"
+Term.create english: "dtr", american: "tr"
+Term.create english: "triple treble", american: "double treble"
+Term.create english: "trtr", american: "dtr"
+Term.create english: "miss", american: "skip"
+Term.create english: "tension", american: "gauge"
+Term.create english: "yarn over hook", american: "yarn over"
+Term.create english: "yoh", american: "yo"
